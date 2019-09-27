0973bdfdbcffc8f2799017ebc956299a124893e0
b19930813 <Frank Tseng>
2019-09-27 17:33:59 +0800

    fix error

--- app/controllers/api/vocabularies_controller.rb
+++ app/controllers/api/vocabularies_controller.rb
@@ -1,20 +1,8 @@
 module API
     class Api::VocabulariesController < ApplicationController
         def index
-            case params['level']
-            when '0'
-              vocabularies = Vocabulary.all
-            when '1'
-                vocabularies = Vocabulary.all
-            when '2'
-                vocabularies = Vocabulary.all
-            when '3'
-                vocabularies = Vocabulary.all
-            when '4'
-                vocabularies = Vocabulary.all
-            when '5'
-                vocabularies = Vocabulary.all
-            end
+            vocabularies = Vocabulary.all
+              puts params['level']
               render json: { vocabularies: vocabularies }
         end
     end
