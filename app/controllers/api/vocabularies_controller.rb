562e8e8dc625476ccfe340c34d622cba54c01d24
b19930813 <Frank Tseng>
2019-10-01 15:39:00 +0800

    單字本完成

--- app/controllers/api/vocabularies_controller.rb
+++ app/controllers/api/vocabularies_controller.rb
@@ -2,7 +2,21 @@ module API
     class Api::VocabulariesController < ApplicationController
         def index
               #puts current_learn_user['id']
-              vocabularies = Vocabulary.all
+              #沒有分類的情況
+              if params['ID'] == '0'
+                if params['searchV'].empty?
+                   vocabularies = Vocabulary.all
+                else
+                    vocabularies = Vocabulary.where("katakana like '%#{params['searchV']}%' OR cnVocabulary like '%#{params['searchV']}%'")
+                end
+              else
+                #分類塞選
+                if params['searchV'].empty?
+                vocabularies = Vocabulary.where("level = #{params['ID']}")
+                else
+                    vocabularies = Vocabulary.where("level = #{params['ID']}").where("katakana like '%#{params['searchV']}%' OR cnVocabulary like '%#{params['searchV']}%'")
+                end
+              end
               render json: { vocabularies: vocabularies }
         end
     end
--- app/javascript/components/LearnVocabulary.js
+++ app/javascript/components/LearnVocabulary.js
@@ -45,143 +45,140 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const MyExpand = props => {
-  
+
   const classes = useStyles();
-  if(props.data.state.vocabularies.length!=0){
-    let lists = props.data.state.vocabularies.map((vocabularies,i) =>
-     <ExpansionPanel key = {vocabularies.id}>
-    <ExpansionPanelSummary
-      expandIcon={<ExpandMoreIcon />}
-      aria-controls="panel1a-content"
-      id="panel1a-header"
-    >
-      <Typography className={classes.heading}>
-        <span>
-        {vocabularies.jpVocabulary}
+    let lists = props.data.state.vocabularies.map((vocabularies, i) =>
+      <ExpansionPanel key={vocabularies.id}>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>
+            <span>
+              {vocabularies.jpVocabulary}
+            </span>
+            <span>
+              [{vocabularies.katakana}] :
         </span>
-        <span>
-        [{vocabularies.katakana}] :
+            <span>
+              {vocabularies.cnVocabulary} /
         </span>
-        <span>
-        {vocabularies.cnVocabulary} /
-        </span>
-        <span>
-        {vocabularies.pos}  
-        </span>
-        </Typography>
-    </ExpansionPanelSummary>
-    <Divider/>
-    <ExpansionPanelDetails>
-      <Typography>
-       [例句]:{vocabularies.jpSentence}
-       <br/>
-       [中譯]:{vocabularies.cnSentence}
-       <br/>
-       <Button variant="contained" color="primary" className={classes.button}>
-        Add
+            <span>
+              {vocabularies.pos}
+            </span>
+          </Typography>
+        </ExpansionPanelSummary>
+        <Divider />
+        <ExpansionPanelDetails>
+          <Typography>
+            [例句]:{vocabularies.jpSentence}
+            <br />
+            [中譯]:{vocabularies.cnSentence}
+            <br />
+            <Button variant="contained" color="primary" className={classes.button}>
+              Add
       </Button>
-      </Typography>
-      
-      
-    </ExpansionPanelDetails>
-  </ExpansionPanel>)
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>)
     return (
-       <div>
+      <div>
         {lists}
-       </div>
+      </div>
     )
-  }
-  else{
-    return null;
-  }
 };
 
-var allVocabularies = null;
-var jpCheck = new RegExp("^[\u0800-\\u4e00]*$"); 
 export default function LearnVocabulary() {
   const classes = useStyles();
   const [values, setValues] = React.useState({
     level: '',
-    input: '',
+    values: '',
+    typingTimeout:0,
+    searchV: '',
   });
- const [state,setState] = React.useState({
-  vocabularies: []
- });
-  const [search,setSearch] = React.useState(0);
+  const [state, setState] = React.useState({
+    vocabularies: []
+  });
+  const [search, setSearch] = React.useState(0);
   const inputLabel = React.useRef(null);
   const [labelWidth, setLabelWidth] = React.useState(0);
   React.useEffect(() => {
     values.level = 0;
-   
     axios
-      .get('/api/vocabularies')
+      .get('/api/vocabularies', {
+        params: {
+          ID: 0,
+          searchV :''
+        }
+        })
       .then(response => {
-
         setState({ vocabularies: response.data.vocabularies });
-        allVocabularies = response.data.vocabularies;
       })
-      
+
   }, []);
 
 
   const handleChange = event => {
- 
     setValues(oldValues => ({
       ...oldValues,
       [event.target.name]: event.target.value,
     }));
-   if(search==''){
-    setState({vocabularies: allVocabularies.filter(vocabulary => {
-      if(event.target.value ==0) return allVocabularies;
-      return vocabulary.level == event.target.value;
-    })});
-  }
-  else{
-    setState({vocabularies: allVocabularies.filter(vocabulary => {
-      if(event.target.value ==0) return vocabulary.cnVocabulary.includes(search)||vocabulary.katakana.includes(search);
-      return vocabulary.level == event.target.value && (vocabulary.katakana.includes(search)==true||vocabulary.cnVocabulary.includes(search));
-    })});
-  }
+    axios
+      .get('/api/vocabularies', {
+        params: {
+          ID: event.target.value,
+          searchV: values.searchV
+        }
+      })
+      .then(response => {
+        setState({ vocabularies: response.data.vocabularies });
+      })
   };
-  
-  const handleSearch = event =>{
-    setSearch(event.target.value);
-     if(event.target.value.length==0){
-       setState({vocabularies: allVocabularies.filter(vocabulary => {
-        if(values.level ==0) return allVocabularies;
-        return vocabulary.level == values.level;
-      })});
-     }
-     else{
-       var temp = allVocabularies.filter(vocabulary => {
-          if(values.level == 0) return vocabulary.katakana.includes(event.target.value)||vocabulary.cnVocabulary.includes(event.target.value);
-          return (vocabulary.katakana.includes(event.target.value)||vocabulary.cnVocabulary.includes(event.target.value)) && values.level == vocabulary.level;
-       })
-       setState({vocabularies: temp});
-     }
+  const handleSearch = event => {
+    let sendV = event.target.value;
+    event.persist();
+    clearTimeout(values.typingTimeout);
+    setValues(oldValues => ({
+      ...oldValues,
+      searchV: sendV,
+      typingTimeout: setTimeout(()=>{
+        axios
+        .get('/api/vocabularies', {
+          params: {
+            ID: values.level,
+            searchV: sendV
+          }
+        })
+        .then(response => {
+          setState({ vocabularies: response.data.vocabularies });
+        })
+     },1000)
+    }));
   }
+
   return (
     <div>
-    <form className={classes.root} autoComplete="off">
-      <FormControl className={classes.formControl}>
-        <InputLabel >單字程度</InputLabel>
-        <Select
-        value={values.level}
-          onChange={handleChange}
-          inputProps={{
-            name: 'level',
-          }}
-        >
-          <MenuItem value={0}>All</MenuItem>
-          <MenuItem value={1}>N1</MenuItem>
-          <MenuItem value={2}>N2</MenuItem>
-          <MenuItem value={3}>N3</MenuItem>
-          <MenuItem value={4}>N4</MenuItem>
-          <MenuItem value={5}>N5</MenuItem> 
-        </Select>
-      </FormControl>
+      <form className={classes.root} autoComplete="off">
+        <FormControl className={classes.formControl}>
+          <InputLabel >單字程度</InputLabel>
+          <Select
+            value={values.level}
+            onChange={handleChange}
+            inputProps={{
+              name: 'level',
+            }}
+          >
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={1}>N1</MenuItem>
+            <MenuItem value={2}>N2</MenuItem>
+            <MenuItem value={3}>N3</MenuItem>
+            <MenuItem value={4}>N4</MenuItem>
+            <MenuItem value={5}>N5</MenuItem>
+          </Select>
+        </FormControl>
 
-      {/* <FormControl className={classes.formControl}>
+        {/* <FormControl className={classes.formControl}>
         <InputLabel >詞性</InputLabel>
         <Select
         value={values.ops}
@@ -195,22 +192,22 @@ export default function LearnVocabulary() {
           <MenuItem value={3}>副詞</MenuItem>
         </Select>
       </FormControl> */}
-      <FormControl className={classes.margin} >
-        <InputLabel>查詢單字</InputLabel>
-        <Input
-          
-          id="input-with-icon-adornment"
-          startAdornment={
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          }
-          onChange = {handleSearch}
-        />
-      </FormControl>
-    </form>
-    <Divider style ={{'marginTop': '20px'}}/>
-    <MyExpand data = {{state}}/>
-  </div>
+        <FormControl className={classes.margin} >
+          <InputLabel>查詢單字</InputLabel>
+          <Input
+
+            id="input-with-icon-adornment"
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            }
+            onChange={handleSearch}
+          />
+        </FormControl>
+      </form>
+      <Divider style={{ 'marginTop': '20px' }} />
+      <MyExpand data={{ state }} />
+    </div>
   );
 }
